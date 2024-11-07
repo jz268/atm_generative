bd71184cd67076ec0e56cf354b5a5c6db7d96b83
Joseph Zhang <jzha@mit.edu>
2024-11-07 08:47:54 -0500

    translation from airport code to nearest weather station id

--- data_utils/weather_data.py
+++ data_utils/weather_data.py
@@ -3,6 +3,68 @@ from pathlib import Path
 import tqdl
 
 import pandas as pd
+from scipy.spatial.distance import cdist
+
+import airportsdata as apd
+
+apd_iata = apd.load('IATA')
+apd_icao = apd.load('ICAO')
+
+import csv
+
+def generate_station_list_csvs():
+    for stem in ("ghcnh-station-list", "lcdv2-station-list"):
+        input_path = f'data_utils/noaa_lut/{stem}.txt'
+        output_path = f'data_utils/noaa_lut/{stem}.csv'
+        with open(input_path, 'r') as f_in:
+            lines = [line[:80].strip().split(maxsplit=4) + [line[80:].strip()]
+                    for line in f_in]
+            with open(output_path, 'w') as f_out:
+                writer = csv.writer(f_out)
+                writer.writerow(('station_id', 'latitude', 'longitude', 'elevation', 'station_name', 'meteostat_id'))
+                writer.writerows(lines)
+
+ghcnh_stations = pd.read_csv(Path(Path(__file__).parent, 'noaa_lut/ghcnh-station-list.csv'))
+lcdv2_stations = pd.read_csv(Path(Path(__file__).parent, 'noaa_lut/lcdv2-station-list.csv'))
+
+def iata_to_icao(code):
+    if code in apd_iata:
+        return apd_iata.get(code)['icao']
+    raise ValueError("invalid iata code")
+
+def icao_to_iata(code):
+    if code in apd_icao:
+        return apd_icao.get(code)['iata']
+    raise ValueError("invalid icao code")
+
+def loc_to_station_id(dataset, lat, lon):
+    stations = ghcnh_stations if dataset == "ghcnh" else lcdv2_stations
+    # technically this is wrong but assuming close enough to not matter
+    dists = abs(stations['latitude']-lat)+abs(stations['longitude']-lon)
+    return stations[dists==dists.min()]['station_id'].values[0]
+
+def iata_to_station_id(dataset, code):
+    if code in apd_iata:
+        lat = apd_iata.get(code)['lat']
+        lon = apd_iata.get(code)['lon']
+        return loc_to_station_id(dataset, lat, lon)
+    raise ValueError("invalid iata code")
+
+def icao_to_station_id(dataset, code):
+    if code in apd_icao:
+        lat = apd_icao.get(code)['lat']
+        lon = apd_icao.get(code)['lon']
+        return loc_to_station_id(dataset, lat, lon)
+    raise ValueError("invalid icao code")
+
+loc_to_lcdv2_id = functools.partial(loc_to_station_id, 'lcdv2')
+loc_to_ghcnh_id = functools.partial(loc_to_station_id, 'ghcnh')
+
+iata_to_lcdv2_id = functools.partial(iata_to_station_id, 'lcdv2')
+iata_to_ghcnh_id = functools.partial(iata_to_station_id, 'ghcnh')
+
+icao_to_lcdv2_id = functools.partial(icao_to_station_id, 'lcdv2')
+icao_to_ghcnh_id = functools.partial(icao_to_station_id, 'ghcnh')
 
 
 class WeatherData:
@@ -11,25 +73,25 @@ class WeatherData:
 
 
 
-# LCDv2: only csv available
-def dl_url_LCDv2(station_id, year):
+# lcdv2: only csv available
+def dl_url_lcdv2(station_id, year):
     return "https://www.ncei.noaa.gov/oa/local-climatological-data/v2/" \
         + f"access/{year}/LCD_{station_id}_{year}.csv"
 
-def out_name_LCDv2(station_name, year):
+def out_name_lcdv2(station_name, year):
     return f"LCD_{station_name}_{year}.csv"
 
-# GHCNh: only parquet or psv available
-def dl_url_GHCNh(station_id, year, ext="parquet"):
+# ghcnh: only parquet or psv available
+def dl_url_ghcnh(station_id, year, ext="parquet"):
     return "https://www.ncei.noaa.gov/oa/global-historical-climatology-network/hourly/" \
         + f"access/by-year/{year}/{ext}/GHCNh_{station_id}_{year}.{ext}"
 
-def out_name_GHCNh(station_name, year, ext="parquet"):
+def out_name_ghcnh(station_name, year, ext="parquet"):
     return f"GHCNh_{station_name}_{year}.{ext}"  
 
 
 def dl_noaa(dataset, out_base_dir, station_id, station_name=None, 
-            start_year=1987, end_year=2023, ext=None, 
+            start_year=1987, end_year=2023, ext="parquet",
             merge_yearly=True, purge_yearly=False):
     # dot data starts on 1987, weather starts way before though
     # also default to ignoring 2024 for now since still incomplete
@@ -38,28 +100,27 @@ def dl_noaa(dataset, out_base_dir, station_id, station_name=None,
     if station_name is None:
         station_name = station_id
 
-    if dataset == "LCDv2":
+    if dataset == "lcdv2":
         ext = "csv"
         dl_url_fn = functools.partial(
-            dl_url_LCDv2, station_id)
+            dl_url_lcdv2, station_id)
         out_name_fn = functools.partial(
-            out_name_LCDv2, station_name)
-        merge_name = out_name_LCDv2(
-            station_name, f'{start_year}-{end_year}', 'csv')
-    elif dataset == "GHCNh":
-        ext = "parquet" if ext is None else ext
+            out_name_lcdv2, station_name)
+        merge_name = out_name_lcdv2(
+            station_name, f'{start_year}-{end_year}')
+    elif dataset == "ghcnh":
         dl_url_fn = functools.partial(
-            dl_url_GHCNh, station_id, ext=ext) 
+            dl_url_ghcnh, station_id, ext=ext) 
         out_name_fn = functools.partial(
-            out_name_GHCNh, station_name, ext=ext)
-        merge_name = out_name_GHCNh(
+            out_name_ghcnh, station_name, ext=ext)
+        merge_name = out_name_ghcnh(
             station_name, f'{start_year}-{end_year}', 'csv')
     else:
-        raise ValueError("dataset must be LCDv2 or GHCNh")
+        raise ValueError("dataset must be lcdv2 or ghcnh")
     
     if ext == "csv":
         pd_read_fn = pd.read_csv
-    if ext == "psv":
+    elif ext == "psv":
         pd_read_fn = functools.partial(pd.read_csv, sep='|')
     elif ext == "parquet":
         pd_read_fn = pd.read_parquet
@@ -92,63 +153,115 @@ def dl_noaa(dataset, out_base_dir, station_id, station_name=None,
         Path.rmdir(out_dir)
     
 
-dl_noaa_LCDv2 = functools.partial(dl_noaa, "LCDv2")
-dl_noaa_GHCNh = functools.partial(dl_noaa, "GHCNh")
+dl_noaa_lcdv2 = functools.partial(dl_noaa, "lcdv2")
+dl_noaa_ghcnh = functools.partial(dl_noaa, "ghcnh")
 
 # https://www.ncei.noaa.gov/oa/local-climatological-data/v2/doc/lcdv2_DOCUMENTATION.pdf
 
 
+
+
+
+
+
+
+
+
+# perhaps this is not worth dealing with, will look at LCD summaries first
+
 # https://www.ncei.noaa.gov/oa/global-historical-climatology-network/hourly/doc/ghcnh_DOCUMENTATION.pdf
 
-dtypes_GHCNh_idx = [
-    "Station_ID",
-    "Station_name",
-    "DATE",
-    "Latitude",
-    "Longitude",
-    "Elevation"
-]
+# dtypes_ghcnh_idx = [
+#     "Station_ID",
+#     "Station_name",
+#     "DATE",
+#     "Latitude",
+#     "Longitude",
+#     "Elevation"
+# ]
 
-dtypes_GHCNh_var = [
-    "temperature", 
-    "dew_point_temperature", 
-    "station_level_pressure", 
-    "sea_level_pressure", 
-    "wind_direction", 
-    "wind_speed", 
-    "wind_gust", 
-    "precipitation", 
-    "relative_humidity", 
-    "wet_bulb_temperature", 
-    "pres_wx_MW1", 
-    "pres_wx_MW2", 
-    "pres_wx_MW3", 
-    "pres_wx_AU1", 
-    "pres_wx_AU2", 
-    "pres_wx_AU3",
-    "pres_wx_AW1", 
-    "pres_wx_AW2", 
-    "pres_wx_AW3",
-    "snow_depth", 
-    "visibility", 
-    "altimeter", 
-    "pressure_3hr_change", 
-    "sky_cover_1", 
-    "sky_cover_2", 
-    "sky_cover_3", 
-    "sky_cover_baseht_1", 
-    "sky_cover_baseht_2", 
-    "sky_cover_baseht_3", 
-    "precipitation_3_hour",
-    "precipitation_6_hour",
-    "precipitation_9_hour",
-    "precipitation_12_hour",
-    "precipitation_15_hour",
-    "precipitation_18_hour",
-    "precipitation_21_hour",
-    "precipitation_24_hour",
-    "remarks"
-]
+# dtypes_ghcnh_var = [
+#     "temperature", 
+#     "dew_point_temperature", 
+#     "station_level_pressure", 
+#     "sea_level_pressure", 
+#     "wind_direction", 
+#     "wind_speed", 
+#     "wind_gust", 
+#     "precipitation", 
+#     "relative_humidity", 
+#     "wet_bulb_temperature", 
+#     "pres_wx_MW1", 
+#     "pres_wx_MW2", 
+#     "pres_wx_MW3", 
+#     "pres_wx_AU1", 
+#     "pres_wx_AU2", 
+#     "pres_wx_AU3",
+#     "pres_wx_AW1", 
+#     "pres_wx_AW2", 
+#     "pres_wx_AW3",
+#     "snow_depth", 
+#     "visibility", 
+#     "altimeter", 
+#     "pressure_3hr_change", 
+#     "sky_cover_1", 
+#     "sky_cover_2", 
+#     "sky_cover_3", 
+#     "sky_cover_baseht_1", 
+#     "sky_cover_baseht_2", 
+#     "sky_cover_baseht_3", 
+#     "precipitation_3_hour",
+#     "precipitation_6_hour",
+#     "precipitation_9_hour",
+#     "precipitation_12_hour",
+#     "precipitation_15_hour",
+#     "precipitation_18_hour",
+#     "precipitation_21_hour",
+#     "precipitation_24_hour",
+#     "remarks"
+# ]
+
+
+# dtypes_ghcnh_var_censored = [
+#     "temperature", 
+#     "dew_point_temperature", 
+#     "station_level_pressure", 
+#     "sea_level_pressure", 
+#     "wind_direction", 
+#     "wind_speed", 
+#     "wind_gust", 
+#     "precipitation", 
+#     "relative_humidity", 
+#     "wet_bulb_temperature", 
+#     # "pres_wx_MW1", # these observations seem to be not reliably there?
+#     # "pres_wx_MW2",
+#     # "pres_wx_MW3", 
+#     # "pres_wx_AU1", 
+#     # "pres_wx_AU2", 
+#     # "pres_wx_AU3",
+#     # "pres_wx_AW1", 
+#     # "pres_wx_AW2", 
+#     # "pres_wx_AW3",
+#     "snow_depth", 
+#     "visibility", 
+#     "altimeter", 
+#     "pressure_3hr_change", 
+#     # "sky_cover_1", # probably less relevant going to ignore at least for now
+#     # "sky_cover_2", 
+#     # "sky_cover_3", 
+#     # "sky_cover_baseht_1", 
+#     # "sky_cover_baseht_2", 
+#     # "sky_cover_baseht_3", 
+#     "precipitation_3_hour",
+#     "precipitation_6_hour",
+#     "precipitation_9_hour",
+#     "precipitation_12_hour",
+#     "precipitation_15_hour",
+#     "precipitation_18_hour",
+#     "precipitation_21_hour",
+#     "precipitation_24_hour",
+#     "remarks"
+# ]
 
 # def clean_noaa():
 
--- noaa_weather_test.py
+++ noaa_weather_test.py
@@ -1,7 +1,17 @@
-from data_utils.weather_data import dl_noaa_GHCNh
+from data_utils.weather_data import *
 from pathlib import Path
 
 
-data_dir = Path('data/noaa_ghcnh').resolve()
+# data_dir = Path('data/noaa_ghcnh').resolve()
 
-dl_noaa_GHCNh(data_dir, "USW00023234", "SFO")
+# dl_noaa_ghcnh(data_dir, "USW00023234", "SFO")
+# dl_noaa_ghcnh(data_dir, "USW00014739", "BOS")
+
+# data_dir = Path('data/noaa_lcdv2').resolve()
+
+# dl_noaa_lcdv2(data_dir, "USW00023234", "SFO")
+# dl_noaa_lcdv2(data_dir, "USW00014739", "BOS")
+
+print(icao_to_lcdv2_id('KSFO'))
+print(icao_to_lcdv2_id('KBOS'))
+print(icao_to_lcdv2_id('KLAX'))
