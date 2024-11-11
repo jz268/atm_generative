5040e65081ca508048c697e370daac8fea6e7545
Joseph Zhang <jzha@mit.edu>
2024-11-11 15:40:26 -0500

    cleanup, move weather cleaning into utils

--- data_utils/weather_noaa_data.py
+++ data_utils/weather_noaa_data.py
@@ -3,7 +3,9 @@ from pathlib import Path
 import tqdl
 
 import pandas as pd
+import numpy as np
 import csv
+import sys
 
 import airportsdata as apd
 
@@ -191,6 +193,131 @@ def dl_noaa_ghcnh_airport(data_dir, code):
 
 
 
+# uses: https://www.kaggle.com/datasets/zhaodianwen/noaaweatherdatajfkairport/data
+
+def tryconvert(value, dt=None):
+    """
+    value -> Value to be converted
+    dt    -> data type to convert to (redundant for now)
+    """
+    try:
+        return np.float64(value)
+    except:
+        return np.nan
+
+def clean_noaa_lcdv2_file(data_path, verbose=False):
+
+    data_path = Path(data_path).resolve()
+
+    import_columns = [  
+        'DATE',
+        'HourlyVisibility',
+
+        'HourlyDryBulbTemperature',
+        'HourlyWetBulbTemperature',
+        'HourlyDewPointTemperature',
+
+        'HourlyRelativeHumidity',
+
+        'HourlyWindSpeed',
+        'HourlyWindDirection',
+
+        'HourlyStationPressure',
+        'HourlySeaLevelPressure',
+        'HourlyPressureTendency',
+        'HourlyAltimeterSetting',
+
+        'HourlyPrecipitation',
+
+        # added (TODO? not sure if relevant)
+        # 'HourlyPresentWeatherType',
+        # 'HourlySkyConditions',
+    ]
+
+# • Snow Depth [cm]
+# • Snow Accumulation [cm]
+
+# • Weather Intensity Code
+# 1:Light, 2:Moderate, 3:Heavy, 4:Vicinity
+
+# • Weather Descriptor Code
+# 1:Shallow, 2:Partial, 3:Patches, 4:Low Drifting, 5:Blowing, 6:Showers, 7:Thunderstorms, 8:Freezing
+
+# • Precipitation Code
+# 1:Drizzle, 2:Rain, 3:Snow, 4:Snow Grains, 5:Ice Crystals,
+# 6:Ice Pellets, 7:Hail, 8:Small Hail and/or Snow Pellets,
+# 9:Unknown Precipitation
+
+# • Obscuration Code
+# 1:Mist, 2:Fog, 3:Smoke, 4:Volcanic Ash, 5:Widespread
+# Dust, 6:Sand, 7:Haze, 8:Spray
+
+# • Other Weather Code
+# 1:Well-Developed Dust/Sand Whirls, 2:Squalls, 3:Funnel
+# Cloud, Tornado, Waterspout, 4:Sandstorm, 5:Duststorm
+
+# • Combination Indicator Code
+# 1:Not part of combined weather elements, 2:Beginning
+# elements of combined weather elements, 3:Combined
+# with previous weather element to form a single weather
+# report
+    
+    # Read data and set datetime index
+    data_weather = pd.read_csv(data_path, parse_dates=['DATE'], usecols=import_columns)
+    data_weather = data_weather.set_index(pd.DatetimeIndex(data_weather['DATE']))
+    data_weather.drop(['DATE'], axis=1, inplace=True)
+    
+    # Replace '*' values with np.nan
+    data_weather.replace(to_replace='*', value=np.nan, inplace=True)
+
+    # Replace trace amounts of precipitation with 0
+    data_weather['HourlyPrecipitation'].replace(to_replace='T', value='0.00', inplace=True) 
+    # Replace rows with tow '.' with np.nan
+    data_weather.loc[data_weather['HourlyPrecipitation'].str.count('\.') > 1, 'HourlyPrecipitation'] = np.nan 
+
+    # Convert to float
+    for i, _ in enumerate(data_weather.columns):
+        data_weather.iloc[:,i] =  data_weather.iloc[:,i].apply(tryconvert)
+
+    # Replace any hourly visibility figure outside these 0-10 bounds
+    data_weather.loc[(data_weather['HourlyVisibility'] > 10) | (data_weather['HourlyVisibility'] < 0), 'HourlyVisibility'] = np.nan
+
+    # Downsample to hourly rows 
+    data_weather = data_weather.resample('60min').last().shift(periods=1) 
+
+    # Interpolate missing values
+    data_weather['HourlyPressureTendency'] = data_weather['HourlyPressureTendency'].fillna(method='ffill') #fill with last valid observation
+    data_weather = data_weather.interpolate(method='linear')
+    data_weather.drop(data_weather.index[0], inplace=True) #drop first row
+
+    # Transform HourlyWindDirection into a cyclical variable using sin and cos transforms
+    data_weather['HourlyWindDirectionSin'] = np.sin(data_weather['HourlyWindDirection'].astype('float')*(2.*np.pi/360))
+    data_weather['HourlyWindDirectionCos'] = np.cos(data_weather['HourlyWindDirection'].astype('float')*(2.*np.pi/360))
+    # data_weather.drop(['HourlyWindDirection'], axis=1, inplace=True)
+
+    # Transform HourlyPressureTendency into 3 dummy variables based on NOAA documentation
+    data_weather['HourlyPressureTendencyIncr'] = [1.0 if x in [0,1,2,3] else 0.0 for x in data_weather['HourlyPressureTendency']] # 0 through 3 indicates an increase in pressure over previous 3 hours
+    data_weather['HourlyPressureTendencyDecr'] = [1.0 if x in [5,6,7,8] else 0.0 for x in data_weather['HourlyPressureTendency']] # 5 through 8 indicates a decrease over the previous 3 hours
+    data_weather['HourlyPressureTendencyCons'] = [1.0 if x == 4 else 0.0 for x in data_weather['HourlyPressureTendency']] # 4 indicates no change during the previous 3 hours
+    # data_weather.drop(['HourlyPressureTendency'], axis=1, inplace=True)
+    data_weather['HourlyPressureTendencyIncr'] = data_weather['HourlyPressureTendencyIncr'].astype(('float32'))
+    data_weather['HourlyPressureTendencyDecr'] = data_weather['HourlyPressureTendencyDecr'].astype(('float32'))
+    data_weather['HourlyPressureTendencyCons'] = data_weather['HourlyPressureTendencyCons'].astype(('float32'))
+
+    # Output csv based on input filename
+    data_dir, data_name = data_path.parent, data_path.stem
+    data_weather.to_csv(data_dir / Path(data_name + '_CLEANED.csv'), float_format='%g')
+
+    if verbose:
+        print("Data successfully cleaned, below are some stats:")
+        print('# of megabytes held by dataframe: ' + str(round(sys.getsizeof(data_weather) / 1000000,2)))
+        print('# of features: ' + str(data_weather.shape[1])) 
+        print('# of observations: ' + str(data_weather.shape[0]))
+        print('Start date: ' + str(data_weather.index[0]))
+        print('End date: ' + str(data_weather.index[-1]))
+        print('# of days: ' + str((data_weather.index[-1] - data_weather.index[0]).days))
+        print('# of months: ' + str(round((data_weather.index[-1] - data_weather.index[0]).days/30,2)))
+        print('# of years: ' + str(round((data_weather.index[-1] - data_weather.index[0]).days/365,2)))
 
 
 
--- weather_noaa_data_test.py
+++ weather_noaa_data_test.py
@@ -12,5 +12,8 @@ data_dir = Path('data/noaa_lcdv2').resolve()
 # dl_noaa_lcdv2(data_dir, "USW00023234", "SFO")
 # dl_noaa_lcdv2(data_dir, "USW00014739", "BOS")
 
-dl_noaa_lcdv2_airport(data_dir, 'LAX')
-dl_noaa_lcdv2_airport(data_dir, 'JFK')
+# dl_noaa_lcdv2_airport(data_dir, 'LAX')
+# dl_noaa_lcdv2_airport(data_dir, 'JFK')
+
+clean_noaa_lcdv2_file('data/noaa_lcdv2/LCD_LAX_1987-2023.csv', verbose=True)
+# clean_noaa_lcdv2_file('data/noaa_lcdv2/LCD_JFK_1987-2023.csv', verbose=True)
